51630432f5d812a72ad1a7e6109fedc52856f37e
half cambodian hacker man <half-kh-hacker@hackery.site>
2020-06-02 04:25:43 +0100

    Avoid redundant reads of the config by passing it in from require_auth

--- wg_manage/__init__.py
+++ wg_manage/__init__.py
@@ -14,14 +14,14 @@ def require_auth(route):
     if request.headers.get("Authorization") != f"Bearer {config['auth_token']}":
       return jsonify({ "error": "Missing or invalid authorization token" }), 403
 
-    return route(*a, **kw)
+    return route(*a, **kw, config=config)
 
   return f
 
 
 @app.route("/add", methods=["POST"])
 @require_auth
-def add():
+def add(config):
   form = request.get_json()
   if form is None:
     form = request.form
@@ -33,8 +33,6 @@ def add():
   if name is None or pk is None:
     return jsonify({ "error": "Missing parameter 'name' or 'pk'." }), 400
 
-  config = read_config()
-
   res = jsonify(add_device(config, name, pk, psk))
   write_config(config)
   return res
@@ -42,7 +40,5 @@ def add():
 
 @app.route("/peers")
 @require_auth
-def peers():
-  config = read_config()
-
+def peers(config):
   return Response(generate_peers_config(config), content_type="text/plain")
